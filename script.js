029916deaad81bd6733e5d428a9f31130dc842dd
Mukesh Kumar <77101515+Programmer-Mukesh@users.noreply.github.com>
2021-07-01 21:36:12 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,8 @@ let hover= document.getElementsByClassName("senior")[0];
 
 hover.addEventListener('mouseover', ()=>{
     dd.style.display="block";
-
 })
 
 hover.addEventListener('mouseout', ()=>{
     dd.style.display="none";
-
 })
